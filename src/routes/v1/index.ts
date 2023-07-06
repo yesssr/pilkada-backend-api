67dd440bcab4f2ad71e4fd52600c0583047f6740
yesssr <adimuhammadyasir93@gmail.com>
2023-07-06 12:33:33 +0700

    add tps & region routes

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -2,9 +2,11 @@ import { Router } from "express";
 import { authController } from "../../middleware/auth";
 import { errorHandler } from "../../middleware/error";
 import { kontestanRouter } from "./kontestan";
+import { regionRouter } from "./region";
 import { usersRouter } from "./users";
 import { rolesRouter } from "./roles";
 import { authRouter } from "./auth";
+import { tpsRouter } from "./tps";
 
 const router = Router();
 
@@ -12,6 +14,8 @@ router.use("/auth", authRouter);
 router.use(authController.auth);
 router.use("/users", usersRouter);
 router.use("/roles", rolesRouter);
+router.use("/tps", tpsRouter);
+router.use("/region", regionRouter);
 router.use("/kontestan", kontestanRouter);
 router.use(errorHandler);
 
