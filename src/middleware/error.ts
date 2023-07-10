c0edf4f6175fab45985db4cd380e9ba21ec4f431
yesssr <adimuhammadyasir93@gmail.com>
2023-07-10 16:46:12 +0700

    fix class local error

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -6,8 +6,12 @@ import {
 } from "objection";
 
 export class localError extends Error {
-  message!: string;
   statusCode!: number;
+
+  constructor(message?: string, statusCode?: number) {
+    super(message);
+    this.statusCode = statusCode!;
+  }
 }
 
 export function errorHandler(
