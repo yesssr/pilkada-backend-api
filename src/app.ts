c79316b029ce91d1b09029ccc7b303176fabcee1
yesssr <adimuhammadyasir93@gmail.com>
2023-07-12 09:37:55 +0700

    add helmet & socket

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,27 @@
+import compression from "compression";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import { Model } from "objection";
 import express from "express";
+import helmet from "helmet";
 import cors from "cors";
 import knex from "./config/config";
 import router from "./routes/v1";
 
 const app = express();
 
+//SOCKET CONFIG
+const httpServer = createServer(app);
+var io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
 Model.knex(knex);
 app.use(cors());
+app.use(helmet());
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
