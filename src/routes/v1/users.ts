3f221e1446f77e68d03f865ae33f9067b5325bca
yesssr <adimuhammadyasir93@gmail.com>
2023-12-18 10:01:16 +0700

    fix

--- src/routes/v1/users.ts
+++ src/routes/v1/users.ts
@@ -6,12 +6,6 @@ const router = Router();
 router.get("/", users.findAllUsers);
 router.get("/kontestan-not-linked", users.findKontestanNotLink);
 router.get("/profile", users.getUserProfile);
-router.get("/auditor/tps/:tps_code", users.findAuditor);
-router.get("/auditor-supervisi/tps/:tps_code", users.findAuditorSupervisi);
-router.get("/auditor-supervisi", users.findAuditorSWithoutTps);
-router.get("/auditor", users.findAuditorWithoutTps);
-router.post("/add-auditor/tps/:tps_code", users.addTpsToAuditor);
-router.delete("/auditor/:auditor_id/tps/:tps_code", users.deleteAuditor);
 router
   .route("/:id")
   .get(users.findByIdUser)
