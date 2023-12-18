216a42c780ef86771cb69d8168f6ebf849538a32
yesssr <adimuhammadyasir93@gmail.com>
2023-12-18 11:15:13 +0700

    fix

--- bundle/model/provinces.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Provinces = void 0;
-const basemodel_1 = require("./basemodel");
-class Provinces extends basemodel_1.BaseModel {
-}
-exports.Provinces = Provinces;
-Provinces.tableName = "provinces";
